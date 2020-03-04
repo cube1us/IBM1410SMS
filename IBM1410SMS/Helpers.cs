fb38be68f85a71f0bb44531d22f49eb2ea213248
Jay Jaeger <cube1us@gmail.com>
2020-03-04 21:55:25 +0000

    Helpers:  added RPQ and SAVE to the list of valid card type for cable/edge connections.
    git-svn-id: svn://localhost/active/IBM1410SMS/trunk@1631 8de05751-f04e-1843-ac3c-a4bcb19ff523

--- IBM1410SMS/Helpers.cs
+++ IBM1410SMS/Helpers.cs
@@ -60,7 +60,7 @@ namespace IBM1410SMS
         //  List of card types that are Cable/Edge Connection card types
 
         internal static string[] cableEdgeConnectionCardTypes = new string[]
-            {"CABL", "CONN"};
+            {"CABL", "CONN", "RPQ", "SAVE"};
 
         private static readonly Helpers instance = new Helpers();
 
